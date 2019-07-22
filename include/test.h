9b9b69eb540eecf6dc30f9c7ad64224b3f3704f5
birdFSS <ffh465031824@163.com>
2019-07-22 13:31:59 +0800

    prepare for this project

---
+++ include/test.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace wd
+{
+
+class test
+{
+public:
+    test(int x) : m_x(x), m_y(0) {}
+    ~test() {}
+
+private:
+    int m_x;
+    int m_y;
+};
+
+
+
+
+
+
+
+}//end of namespace wd
+
---
+++ src/test.cc
@@ -0,0 +1,19 @@
+#include "../include/test.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+    wd::test t(1);
+    cout << " ok "<< endl;
+    return 0;
+}
+
