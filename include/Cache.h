6133c12e71fafaedabdf1d3b76705e755f8d39e9
birdFSS <ffh465031824@163.com>
2019-07-28 00:25:35 +0800

    LRU in Cache

--- include/Cache.h
+++ include/Cache.h
@@ -1,11 +1,23 @@
 #pragma once
 #include <unordered_map>
+#include <list>
+#define CACHE_SIZE 5
 
 namespace wd
 {
 
+
 class Cache
 {
+    struct CacheNode
+    {
+        CacheNode(const std::string& key,const std::string value) :
+            m_key(key),
+            m_value(value)
+        {}
+        std::string m_key;
+        std::string m_value;
+    };
 public:
     Cache();
     Cache(const Cache& cache);
@@ -14,11 +26,10 @@ public:
     void readFromFile(const std::string& fileName);
     void writeToFile(const std::string& fileName);
     void update(const Cache& rhs);
-    const std::unordered_map<std::string, std::string>&
-    getHashMap() const 
-    { return m_hashMap; }
+
 private:
-    std::unordered_map<std::string, std::string> m_hashMap;
+    std::list<CacheNode> m_list;
+    std::unordered_map<std::string, std::list<CacheNode>::iterator> m_hashMap;
 };
 
 
--- src/Cache.cc
+++ src/Cache.cc
@@ -16,19 +16,49 @@ Cache::Cache()
 }
 
 Cache::Cache(const Cache& cache) :
-    m_hashMap(cache.getHashMap().begin(), cache.getHashMap().end())
+    m_list(cache.m_list),
+    m_hashMap(cache.m_hashMap)
 {
 
 }
 
 Cache::~Cache()
 {
-
+    //回收空间
+    auto iter = m_hashMap.begin();
+    while(iter != m_hashMap.end())
+    {
+        delete &*iter->second;
+        ++iter;
+    }
 }
-//后面加上lru算法
+
+//LRU算法
 void Cache::addElement(const std::string& key, const std::string &value)
 {
-    m_hashMap.insert(make_pair(key, value));
+    //查找关键字是否在hash表中
+    auto iter = m_hashMap.find(key);
+    if(iter != m_hashMap.end())
+    {
+        //在hash表中，将其从链表中取出来，再插入链表头
+        m_list.erase(iter->second);
+        m_list.push_front(*iter->second);
+    }else{
+        //不再hash表中，新建一个节点，将节点放在链表头，如果Cache满了，
+        //则将链表尾结点删除
+        CacheNode* pNode = new CacheNode(key, value);
+        m_list.push_front(*pNode);
+        m_hashMap.insert(make_pair(key, m_list.begin()));
+        if(m_list.size() > CACHE_SIZE)
+        {
+            string key = m_list.back().m_key;
+            m_list.pop_back();
+            auto iter = m_hashMap.find(key);
+            delete &*iter->second;
+            m_hashMap.erase(iter);
+        }
+
+    }
 }
 
 void Cache::readFromFile(const std::string& fileName)
@@ -41,16 +71,19 @@ void Cache::readFromFile(const std::string& fileName)
     }
 }
 
-
+//反向写入文件，这样读入的时候顺序是对的
+//即读出时的队头的是与写入前一样的
 void Cache::writeToFile(const std::string& fileName)
 {
     ofstream ofs(fileName);
-    for(auto it = m_hashMap.begin(); it != m_hashMap.end(); ++it)
+    for(auto it = m_list.rbegin(); it != m_list.rend(); ++it)
     {
-        ofs << it->first << " " << it->second << endl;
+        ofs << it->m_key << " " << it->m_value << endl;
     }
 }
 
+
+//current 
 void Cache::update(const Cache& rhs)
 {
     for(auto it = rhs.getHashMap().cbegin(); it != m_hashMap.cend(); ++it)
